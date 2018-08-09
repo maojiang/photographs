b7adcc540f85e23aeac8902cf7ae411879507bdb
maojiang <568213372@qq.com>
2018-08-09 15:58:59 +0800

    create project
    创建项目

---
+++ photograph/src/main/java/org/dmj/sch/PhotographApplication.java
@@ -0,0 +1,12 @@
+package org.dmj.sch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotographApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotographApplication.class, args);
+    }
+}
